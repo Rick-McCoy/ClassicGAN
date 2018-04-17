1eff5102bd0078b661bf29729ee91d729a118b8e
Rick-McCoy <rickmccoy3141@gmail.com>
2018-04-17 14:35:49 +0900

    Adding import tensorflow to Data.

--- Data.py
+++ Data.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import tensorflow as tf
 import pretty_midi as pm
 import numpy as np
 import pathlib
