e4600a76868eece2ebba19591e47dfe95e9b378a
Rick-McCoy <rickmccoy3141@gmail.com>
2018-05-02 09:15:12 +0900

    Channels first for max pooling 3d.

--- ClassicGAN.py
+++ ClassicGAN.py
@@ -85,9 +85,11 @@ def main():
         # shape: [1, CHANNEL_NUM, CLASS_NUM, INPUT_LENGTH, 1]
         real_input_2_image = tf.layers.max_pooling3d(inputs=real_input_3_image, pool_size=[2, 2, 1], \
                                                         strides=(2, 2, 1), padding='same', \
+                                                        data_format='channels_first', \
                                                         name='real_input_2_image')
         real_input_1_image = tf.layers.max_pooling3d(inputs=real_input_2_image, pool_size=[2, 2, 1], \
                                                         strides=(2, 2, 1), padding='same', \
+                                                        data_format='channels_first', \
                                                         name='real_input_1_image')
         for i in range(CHANNEL_NUM):
             tf.summary.image('real_input_1_' + str(i), real_input_1_image[:, i])
